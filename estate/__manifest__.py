dcfa92a9bf25faa5f29055aa73f5c3c78b2146f8
olga <olgakrikunova12@gmail.com>
2025-06-08 11:24:35 +0400

    [ADD][1] add estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Estate",
+    "author": "Olga Krikunova",
+    "version": "16.0.0.0.0",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
+    "installable": True,
+    "application": True,
+}

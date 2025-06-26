c1fbc98d0dd7683537053ffdb1c6a31c25588ff1
olga <olgakrikunova12@gmail.com>
2025-06-26 17:43:07 +0400

    [ADD][7]  new module estate_account added

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Estate Account",
+    "author": "Olga Krikunova",
+    "version": "18.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["estate", "account"],
+    "installable": True,
+    "application": False,
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,36 @@
+from odoo import Command, models
+
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def estate_property_action_sold(self):
+        for property in self:
+            property_name = property.name
+            commission = property.selling_price * 0.06 if property.selling_price else 0.0
+            admin_fee = 100.0
+
+            move_vals = {
+                "partner_id": property.buyer_id.id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": [
+                    Command.create(
+                        {
+                            "name": f" {property_name} (commission 6% of sale price)",
+                            "quantity": 1,
+                            "price_unit": commission,
+                        }
+                    ),
+                    Command.create(
+                        {
+                            "name": "Administrative fee",
+                            "quantity": 1,
+                            "price_unit": admin_fee,
+                        }
+                    ),
+                ],
+            }
+
+            self.env["account.move"].create(move_vals)
+
+        return super().estate_property_action_sold()

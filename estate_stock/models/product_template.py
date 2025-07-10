4b12ed27756e04d8c2d9d0666926ae60bb89d977
olga <olgakrikunova12@gmail.com>
2025-07-10 15:10:40 +0400

    [ADD][1] Add stock-module added filter "Is Low Stock"

---
+++ estate_stock/models/product_template.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    is_stock_low = fields.Boolean(compute="_compute_is_stock_low")
+
+    @api.depends()
+    def _compute_is_stock_low(self):
+        for record in self:
+            if record.qty_available < 10:
+                record.is_stock_low = True
+            else:
+                record.is_stock_low = False

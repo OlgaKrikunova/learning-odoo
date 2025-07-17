e36c2ccb681610d39472d8e8a8fcc28e3dccacb8
olga <olgakrikunova12@gmail.com>
2025-07-17 17:26:19 +0400

    [ADD][1] Added "Manager's comment" field to the client card module "estate_partner"

---
+++ estate_partner/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Estate",
+    "author": "Olga Krikunova",
+    "version": "18.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["estate", "base", "partner_autocomplete"],
+    "data": [
+        "views/estate_partner_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ estate_partner/models/estate_partner.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    manager_comment = fields.Text(string="Manager's Comment")

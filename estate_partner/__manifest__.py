7961577e4153ad6e87ab4b51b3131490020f8cf6
olga <olgakrikunova12@gmail.com>
2025-07-21 12:45:43 +0400

    changes after code review

--- estate_partner/__manifest__.py
+++ estate_partner/__manifest__.py
@@ -1,9 +1,9 @@
 {
-    "name": "Estate",
+    "name": "Estate Partner",
     "author": "Olga Krikunova",
     "version": "18.0.1.0.0",
     "license": "LGPL-3",
-    "depends": ["estate", "base", "partner_autocomplete"],
+    "depends": ["estate", "partner_autocomplete"],
     "data": [
         "views/estate_partner_views.xml",
     ],

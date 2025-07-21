1a47aab4348a36f08046205b84b0710f5e180673
olga <olgakrikunova12@gmail.com>
2025-07-21 12:48:36 +0400

    changes after code review

--- estate_stock/__manifest__.py
+++ estate_stock/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Estate Stock",
     "author": "Olga Krikunova",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "license": "LGPL-3",
     "depends": ["stock"],
     "installable": True,

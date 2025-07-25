041e3023b0e8e0520acb96b945aac5bc5d29247a
olga <olgakrikunova12@gmail.com>
2025-07-25 16:47:16 +0400

    [ADD][4] Added tooltip (help) and added color background to orders if the amount is more than 1000

--- estate_account/models/account_move.py
+++ estate_account/models/account_move.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    total_with_tax = fields.Monetary(compute="_compute_total_with_tax")
+    total_with_tax = fields.Monetary(compute="_compute_total_with_tax", help="This is an amount including taxes.")
 
     @api.depends("amount_total", "amount_tax")
     def _compute_total_with_tax(self):

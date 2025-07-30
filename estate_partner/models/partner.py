0638a15d6c7a042957988601dad376fa5e2876f5
olga <olgakrikunova12@gmail.com>
2025-07-30 14:12:31 +0400

    [ADD][9] Prevent deleting a client if he has orders

--- estate_partner/models/partner.py
+++ estate_partner/models/partner.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class ResPartner(models.Model):
@@ -7,3 +8,9 @@ class ResPartner(models.Model):
     manager_comment = fields.Text(string="Manager's Comment")
 
     is_vip = fields.Boolean(string="VIP client")
+
+    @api.ondelete(at_uninstall=False)
+    def _check_partner_orders(self):
+        for partner in self:
+            if partner.sale_order_ids:
+                raise UserError(_("It is not possible to delete a client with orders."))
